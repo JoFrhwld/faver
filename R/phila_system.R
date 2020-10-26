f4b75ebf02d4d35baf1b21bf148d37c1dbafcc4e
Josef Fruehwald <JoFrhwld@gmail.com>
2020-10-26 15:46:45 -0400

    moved vowel recoding into its own incorporation function

--- R/phila_system.R
+++ R/phila_system.R
@@ -11,7 +11,6 @@
 #' @importFrom dplyr mutate
 #' @importFrom dplyr case_when
 #' @importFrom dplyr lead
-#' @importFrom dplyr left_join
 #' @importFrom stringr str_c
 #' @export
 phila_ae <- function(df){
@@ -51,14 +50,115 @@ phila_ae <- function(df){
                                   vclass == "æ" &
                                     post_phone_word %in% c('M', 'N', 'S', 'TH', 'F') &
                                     post_syl_part == "coda"~ "æh",
-                                  TRUE ~ vclass)) ->ae_coding
+                                  TRUE ~ vclass)) %>%
+    incorporate_recoding(df) -> out
 
-  ae_coding %>%
-    select(speakerid, word_n, phone_n, new_vclass) ->ae_coded
+  return(out)
+}
 
+
+
+#' marry fixing
+#'
+#' input expects basic vclass coding
+#'
+#' todo: check for vclass column. Throw error otherwise
+#'
+#' Dramatically incomplete!
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select
+#' @importFrom dplyr group_by
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr case_when
+#' @importFrom dplyr lead
+#' @importFrom stringr str_c
+#' @export
+phila_marry <- function(df){
 
   df %>%
-    left_join(ae_coded, by = c("speakerid", "phone_n", "word_n")) %>%
+    filter(grepl("ARR", toupper(word_label))) %>%
+    group_by(speakerid, word_n) %>%
+    filter(any(phone_label == "EH1")) %>%
+    mutate(new_vclass = case_when(phone_label == "EH1" &
+                                    lead(phone_label) == "R" &
+                                    grepl("[AEIOU]", lead(phone_label, n = 2)) ~ "æ",
+                                  TRUE~vclass))%>%
+    incorporate_recoding(df) -> out
+
+  return(out)
+}
+
+
+#' low-back vowels fixing
+#'
+#' input expects basic vclass coding
+#'
+#' todo: check for vclass column. Throw error otherwise
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select
+#' @importFrom dplyr group_by
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr case_when
+#' @importFrom dplyr lead
+#' @importFrom dplyr left_join
+#' @importFrom stringr str_c
+#' @export
+phila_lowback <- function(df){
+
+  thought <- c('LAW', 'LAWS', "LAW'S", 'LAWFUL', 'UNLAWFUL', 'DOG', 'DOGS', "DOG'S",
+               'DOGGED','ALL', "ALL'S", 'CALL', 'CALLS', "CALL'S", 'CALLING',
+               'CALLED', 'FALL', 'FALLS', "FALL'S", 'FALLING',
+               'AUDIENCE', 'AUDIENCES', "AUDIENCE'S", 'ON',
+               'ONTO', 'GONNA', 'GONE', 'BOSTON', "BOSTON'S",
+               'AWFUL', 'AWFULLY', 'AWFULNESS', 'AWKWARD',
+               'AWKWARDLY', 'AWKWARDNESS', 'AWESOME', 'AUGUST',
+               'COUGH', 'COUGHS', 'COUGHED', 'COUGHING')
+
+  lot <- c('CHOCOLATE', 'CHOCOLATES', "CHOCOLATE'S", 'WALLET', 'WALLETS',
+           'WARRANT', 'WARRANTS','WATCH', 'WATCHES', 'WATCHED',
+           'WATCHING', 'WANDER', 'WANDERS', 'WANDERED', 'WANDERING',
+           'CONNIE', 'CATHOLICISM', 'WANT', 'WANTED', 'PONG', 'GONG',
+           'KONG', 'FLORIDA', 'ORANGE', 'HORRIBLE', 'MAJORITY')
+
+  df %>%
+    group_by(speakerid, word_n) %>%
+    filter(any(grepl("AA", phone_label)) , word_label %in% thought) %>%
+    mutate(new_vclass = case_when(vclass == "o" ~ "oh",
+                                  TRUE ~ vclass)) -> new_thought
+
+  df %>%
+    group_by(speakerid, word_n) %>%
+    filter(any(grepl("AO", phone_label)) , word_label %in% lot) %>%
+    mutate(new_vclass = case_when(vclass == "oh" ~ "o",
+                                  TRUE ~ vclass)) -> new_lot
+
+  bind_rows(new_thought, new_lot) %>%
+    incorporate_recoding(df) -> out
+
+  return(out)
+}
+
+
+#' Incorporate Recoding
+#'
+#' incorporate recoding of new_vclass into vclass
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr case_when
+#' @importFrom dplyr select
+incorporate_recoding <- function(recoded, df){
+  recoded %>%
+    select(speakerid, word_n, phone_n, new_vclass) -> recoded
+
+  df %>%
+    left_join(recoded, by = c("speakerid", "phone_n", "word_n")) %>%
     mutate(vclass = case_when(!is.na(new_vclass) & new_vclass != vclass ~ new_vclass,
                               TRUE ~ vclass)) %>%
     select(-new_vclass) -> out
